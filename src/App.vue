52190d206fd0ed0c49aa1fd08dade82c47c88b75
Belen Garcia Martinez <belengm.dev@gmail.com>
2024-05-27 12:06:55 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div class="">
+      <nav class="flex w-full justify-around text-[red]">
+        <RouterLink to="/">Home</RouterLink>
+       <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
