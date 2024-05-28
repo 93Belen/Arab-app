d9f36e153d696e88461c16ae04f3477d0cb014cf
Belen Garcia Martinez <belengm.dev@gmail.com>
2024-05-28 12:25:06 -0500

    card flip

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,17 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
+<div class="flex justify-center flex-col gap-10 pt-5 px-5">
   <header>
     <div class="">
-      <nav class="flex w-full justify-around text-[red]">
+      <nav class="flex w-full justify-around">
         <RouterLink to="/">Home</RouterLink>
-       <RouterLink to="/about">About</RouterLink>
+       <RouterLink to="/about">Statistics</RouterLink>
       </nav>
     </div>
   </header>
-
-  <RouterView />
+  <div class="flex items-center justify-center content-center w-full">
+    <RouterView />
+  </div>
+</div>
 </template>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,4 +1,43 @@
+<script setup>
+import { ref } from 'vue'
+const isFlipped = ref(false)
+const flipCard = () => {
+  isFlipped.value = !isFlipped.value
+}
+</script>
+
 <template>
-  <h1>Home</h1>
+<div class="maincontainer relative w-[250px] h-[320px] rounded-lg">
+<div class="thecard border-2 text-center rounded-lg border-black absolute w-full h-full"
+@click="flipCard"
+ :class="{ 'flipped': isFlipped }"
+>
+    <div class="thefront absolute w-full h-full bg-[yellow]">
+      <p class="text-6xl">Front</p>
+    </div>
+    <div class="theback absolute w-full h-full bg-[lightblue]">
+      <p class="text-6xl">Back</p>
+    </div>
+  </div>
+</div> 
 </template>
 
+<style lang="css" scoped>
+
+.thecard {
+transform-style: preserve-3d;
+transition: all 0.5s ease;
+}
+.thecard.flipped {
+  transform: rotateY(180deg);
+}
+.thefront {
+  backface-visibility: hidden;
+}
+.theback {
+  backface-visibility: hidden;
+  transform:  rotateY(180deg);
+}
+
+
+</style>
