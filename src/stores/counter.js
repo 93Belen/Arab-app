45ad78b8d76d265d3c08d8e1d3d43f8e5f098205
Belen Garcia Martinez <belengm.dev@gmail.com>
2024-05-29 06:59:39 -0500

    added alphabet

--- src/stores/counter.js
+++ src/stores/counter.js
@@ -4,7 +4,37 @@ export const useCounterStore = defineStore('counter', {
   state: () => ({ 
     isFlipped: false,
     right: 0,
-    wrong: []
+    wrong: [],
+    alphabet: [
+    ["ا", "Alif"],
+    ["ب", "Ba"],
+    ["ت", "Ta"],
+    ["ث", "Za"],
+    ["ج", "Yim"],
+    ["ح", "Ha"],
+    ["خ", "Ja"],
+    ["د", "Dal"],
+    ["ذ", "Dhal"],
+    ["ر", "Ra"],
+    ["ز", "Za"],
+    ["س", "Sin"],
+    ["ش", "Shin"],
+    ["ص", "Saad"],
+    ["ض", "Daad"],
+    ["ط", "Taa"],
+    ["ظ", "Zaa"],
+    ["ع", "Ayn"],
+    ["غ", "Ghayn"],
+    ["ف", "Fa"],
+    ["ق", "Qaf"],
+    ["ك", "Kaf"],
+    ["ل", "Lam"],
+    ["م", "Mim"],
+    ["ن", "Nun"],
+    ["ه", "Ha"],
+    ["و", "Waw"],
+    ["ي", "Ya"],
+    ]
    }),
   actions: {
     flip(param) {
@@ -17,4 +47,10 @@ export const useCounterStore = defineStore('counter', {
       this.wrong.push(param)
     }
   },
+  getters: {
+    getRandomLetter(state) {
+      const randomIndex = Math.floor(Math.random() * state.alphabet.length);
+      return state.alphabet[randomIndex]
+    }
+  }
 })
