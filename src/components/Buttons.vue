739a4b07cc3089664c75ac46000b345e9ca5d1fd
Belen Garcia Martinez <belengm.dev@gmail.com>
2024-05-29 06:28:40 -0500

    added incons

--- src/components/Buttons.vue
+++ src/components/Buttons.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { useCounterStore } from '../stores/counter'
 import { ref, watch } from 'vue'
+import { Icon } from '@iconify/vue';
 
 const store = useCounterStore()
 const isFlipped = ref(store.isFlipped)
@@ -12,7 +13,8 @@ watch(() => store.isFlipped, (newValue) => {
 </script>
 
 <template>
-<div v-if="isFlipped">
-  <p>Hola</p>
+<div v-if="isFlipped" class="flex justify-around py-5">
+  <button class="rounded-full"><Icon icon="gravity-ui:circle-chevron-down-fill" width="50px" height="50px"  style="color: #77bb41" /></button>
+  <button class="rounded-full"><Icon icon="gravity-ui:circle-xmark-fill" width="50px" height="50px"  style="color: #831100" /></button>
 </div>
 </template>
