d542f73dcadba3bf5eb19edc41f0a7192ea61294
Belen Garcia Martinez <belengm.dev@gmail.com>
2024-05-29 08:51:35 -0500

    statistics

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,21 @@
+<script setup>
+import { useCounterStore } from "../stores/counter";
+
+const store = useCounterStore()
+const errors = store.wrong
+
+</script>
+
+
+
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about w-full text-center">
+    <h1>Erros:</h1>
+    <ul class="flex flex-col">
+      <li class="flex w-full justify-around" :class="{ 'bg-[#E3E3E3]': index % 2 === 0 }" v-for="(error, index) in errors" :key="error">
+        <p>{{ error[0] }}</p>
+        <p>{{ error[1] }}</p>
+      </li>
+    </ul>
   </div>
 </template>
