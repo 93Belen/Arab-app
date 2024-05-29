47b2181b5c92b0d71509359eb8d5809ca7db2b1b
Belen Garcia Martinez <belengm.dev@gmail.com>
2024-05-29 09:51:57 -0500

    color code

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -23,8 +23,10 @@ const howManyTimes = (item) => {
       <li class="flex w-full justify-around"
       :class="{
       'bg-[#E3E3E3]': index % 2 === 0,
-      'bg-[red]' : howManyTimes(error) > 2,
-      'bg-[orange]': howManyTimes(error) > 1 && howManyTimes(error) <= 2
+      'bg-[#EBAA59]': howManyTimes(error) > 1 && howManyTimes(error) <= 2,
+      'bg-[#E48E50]': howManyTimes(error) > 2 && howManyTimes(error) <= 3,
+      'bg-[#D75B3D]': howManyTimes(error) > 3 && howManyTimes(error) <= 4,
+      'bg-[#CC302E]': howManyTimes(error) > 4 && howManyTimes(error) <= 5,
       }"
        v-for="(error, index) in uniqueErros" :key="error">
         <p>{{ error[0] }}</p>
