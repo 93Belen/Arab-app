97cf890f7598e522069697bccc1c1a4c565594d5
Belen Garcia Martinez <belengm.dev@gmail.com>
2024-06-01 14:36:12 -0500

    added level indicator

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,6 +5,7 @@ import { useCounterStore } from '../stores/counter'
 import { onMounted, ref, watch, defineComponent } from 'vue'
 import { VOnboardingWrapper, useVOnboarding } from 'v-onboarding'
 import 'v-onboarding/dist/style.css'
+import { Icon } from '@iconify/vue';
 
 const steps = [
   { attachTo: { element: '#card' }, content: { description: "Guess the letter displayed here! When you're ready, click the card to reveal the answer." } },
@@ -63,5 +64,9 @@ onMounted(() => {
        }" 
     :value="percentage" max="100" />
     <Buttons id="buttons" :letter='letter' :getLetter="getLetter" />
+    <div v-if="(store.right.length === 25 || store.right.length === 40) && !store.isFlipped && store.percentage > 40" class="text-[#77bb41] text-center">
+      <Icon class="m-auto" icon="line-md:circle-twotone-to-confirm-circle-twotone-transition" width="50px" height="50px"  style="color: #77bb41" />
+      <h2>Next Level!</h2>
+    </div>
   </main>
 </template>
